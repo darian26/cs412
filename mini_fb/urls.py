828a2ecb8d28d47c0ead83d5ee60a581a1cc8e2e
DarianC26 <dariancheung@gmail.com>
2024-10-07 16:34:16 -0400

    added views and templates for mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,11 +2,13 @@
 mini_fb/urls.py
 description: the app-specific URLS for the mini_db application
 '''
-
 from django.urls import path
 from django.conf import settings
 from . import views
 
 # create a list of URLs for this app:
 urlpatterns = [
+    # path(url, view, name)
+    path(r'', views.ShowAllView.as_view(), name="show_all"), 
+   
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+# view/views.py
+# Define the views for the blog app:
+#from django.shortcuts import render
+from .models import Profile
+from django.views.generic import ListView
+class ShowAllView(ListView):
+    '''Create a subclass of ListView to display all blog profiles.'''
+    model = Profile # retrieve objects of type Article from the database
+    template_name = 'mini_fb/show_all.html'
+    context_object_name = 'profiles' # how to find the data in the template file
