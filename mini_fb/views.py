26d17125a6bc1a794d1612515a479b2d06cc39dc
DarianC26 <dariancheung@gmail.com>
2024-10-07 16:44:30 -0400

    changing html file name

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+'''
+View file that renders the templates
+'''
 
 # view/views.py
 # Define the views for the blog app:
@@ -8,6 +9,6 @@ from .models import Profile
 from django.views.generic import ListView
 class ShowAllView(ListView):
     '''Create a subclass of ListView to display all blog profiles.'''
-    model = Profile # retrieve objects of type Article from the database
-    template_name = 'mini_fb/show_all.html'
+    model = Profile # retrieve objects of type Profile from the database
+    template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
